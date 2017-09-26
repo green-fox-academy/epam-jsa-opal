38959b00fb61e819752c49cdd77fcfcbb524b4b6
zhengnan zhang <zhengnanzhang@yanans-MBP.princeton.epam.com>
2017-09-26 17:23:38 +0800

    Back end serves static files

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 'use strict';
 var express = require('express');
 var app = express();
+app.use(express.static('src'));
+app.get('/', (req, res) => {
+  res.send('src/index.html');
+})
 app.listen(3000, () => {
   console.log('listening to port:3000');
 });
