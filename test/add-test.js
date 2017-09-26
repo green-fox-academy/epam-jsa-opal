90c56b1be83e3b9235e19b9884b19c3110be94fb
zhengnan zhang <zhengnanzhang@yanans-MBP.princeton.epam.com>
2017-09-26 14:47:47 +0800

    create-dummy-test-for-back-end

---
+++ test/add-test.js
@@ -0,0 +1,7 @@
+describe("my great and huge math lib", function () {
+  it("should perfectly complete complex addition", function () {
+   var result = add(3, 6);
+   expect(result).toEqual(9);
+  });
+  });
+  
