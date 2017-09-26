192c080af9541f9bea8bea9bb9d7308aa7018b2b
ZoeCN <290241128@qq.com>
2017-09-26 14:46:25 +0800

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,4 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-
 module.exports = {
-
-  entry: path.resolve(__dirname, 'src') + '/app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist') + '/app',
     filename: 'bundle.js',
@@ -29,8 +24,5 @@ module.exports = {
         ]
       }
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin([__dirname + 'dist'])
-  ],
+  }
 };
