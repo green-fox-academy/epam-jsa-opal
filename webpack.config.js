39fff2894dd7083bc0aa77457065158e01d9c430
ZoeCN <290241128@qq.com>
2017-09-26 14:01:16 +0800

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+
+  entry: path.resolve(__dirname, 'src') + '/app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist') + '/app',
+    filename: 'bundle.js',
+    publicPath: '/app/'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.[css|scss]$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin([__dirname + 'dist'])
+  ],
+};
