1eb6dcabb50af20cf3feb99c8d1114c26e73d95a
zhengnan zhang <zhengnanzhang@yanans-MBP.princeton.epam.com>
2017-09-26 16:17:23 +0800

    fixed

--- test/add-test.js
+++ test/add-test.js
@@ -1,6 +1,5 @@
 describe("my great and huge math lib", function () {
   it("should perfectly complete complex addition", function () {
-   expect(true).toEqual(true);
+    expect(true).toEqual(true);
   });
-  });
-  
+});
