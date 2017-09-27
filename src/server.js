9bfd690651b7531de28862afa51cdd212cde7ca5
ZoeCN <290241128@qq.com>
2017-09-27 12:26:43 +0800

    Separate dev and prod webpack configuration

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 'use strict';
 var express =  require('express');
 var app = express();
-app.use(express.static('src'));
+app.use(express.static('dist'));
 app.get('/', (req, res) => {
-  res.send('src/index.html');
+  res.send('dist/index.html');
 })
 app.listen(3000, () => {
   console.log('listening to port:3000');
