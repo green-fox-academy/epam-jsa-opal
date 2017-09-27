acd092cfdcb9ebfe10db7cf9a6a01150310a358e
zhengnan zhang <zhengnanzhang@yanans-MBP.princeton.epam.com>
2017-09-27 10:35:33 +0800

    create hello world application

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static('src'));
+app.use(express.static('dist'));
 app.get('/', (req, res) => {
   res.send('src/index.html');
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const path = require('path');
 module.exports = {
+  entry: path.resolve(__dirname, 'src') + '/index.js',
   output: {
     path: path.resolve(__dirname, 'dist') + '/app',
     filename: 'bundle.js',
