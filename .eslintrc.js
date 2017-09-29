bea98e036d3b994a8f3b60f59dcd8c9d9e341c75
Haochen Li <haochenli@appledeMBP-2.princeton.epam.com>
2017-09-29 11:32:40 +0800

    diff enviorment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  'extends': 'google',
+  'extends': ['eslint:recommended', 'google'],
   'parser': 'babel-eslint',
   'env': {
-    'browser': true,
+    'es6': true,
     'node': true,
   },
   'rules': {
     'require-jsdoc': 0,
     'react/jsx-uses-vars': 2,
     'react/jsx-uses-react': 2,
-    'indent' : ['error',2],
+    'indent': ['error', 2],
   },
   'plugins': [
     'react',
