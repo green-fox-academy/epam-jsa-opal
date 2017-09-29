f2346f7d6e0f71bdfebcfd8f8071d0fbe1b6207a
zhengnan zhang <zhengnanzhang@yanans-MBP.princeton.epam.com>
2017-09-29 10:56:36 +0800

    hello world application

--- src/client/index.js
+++ src/client/index.js
@@ -2,9 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Hello';
-// let React = require('react');
-// let ReactDOM = require('react-dom');
-// let App = require('./components/Hello');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
