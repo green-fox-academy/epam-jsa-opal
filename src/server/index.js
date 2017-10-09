b7e29f0c71744803f0164b41e834cd9d7bcdb7f1
zhengnan zhang <zhengnanzhang@yanans-MBP.princeton.epam.com>
2017-10-09 11:46:56 +0800

    OPL-24

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,17 @@ const app = express();
 
 app.use(express.static('dist'));
 app.get('/heartbeat', (req, res) => {
-  res.json({status: 'ok'});
+  var response;
+  var MongoClient = require('mongodb').MongoClient;
+  var url = 'mongodb://localhost:27017/epam';
+  MongoClient.connect(url,function(err,db) {
+    var adminDb = db.admin();
+    adminDb.serverStatus(function(err,info) {
+      console.log(info.version);
+      res.json(info.version);
+      db.close();
+    });
+  });
 });
 
 /* eslint no-console: "off" */
