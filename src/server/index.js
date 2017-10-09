a02cb1015958b875b2f3d3dfb01c10bf73b06035
zhengnan zhang <zhengnanzhang@yanans-MBP.princeton.epam.com>
2017-10-09 15:04:27 +0800

    chang eslint

--- src/server/index.js
+++ src/server/index.js
@@ -6,16 +6,15 @@ const app = express();
 console.log(process.env.DB_URL);
 app.use(express.static('dist'));
 app.get('/heartbeat', (req, res) => {
-  var response;
-  var MongoClient = require('mongodb').MongoClient;
-  var protocol = process.env.DB_PROTOCOL;
-  var host = process.env.DB_HOST;
-  var port = process.env.DB_PORT;
-  var name = process.env.DB_NAME;
-  var url = protocol+'://'+host+':'+port+'/'+name;
-  MongoClient.connect(url,function(err,db) {
-    var adminDb = db.admin();
-    adminDb.serverStatus(function(err,info) {
+  let MongoClient = require('mongodb').MongoClient;
+  let protocol = process.env.DB_PROTOCOL;
+  let host = process.env.DB_HOST;
+  let port = process.env.DB_PORT;
+  let name = process.env.DB_NAME;
+  let url = protocol+'://'+host+':'+port+'/'+name;
+  MongoClient.connect(url, function(err, db) {
+    let adminDb = db.admin();
+    adminDb.serverStatus(function(err, info) {
       console.log(info.version);
       res.json(info.version);
       db.close();
