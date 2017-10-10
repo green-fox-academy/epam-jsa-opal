f7ed5d2e70312b224bd7b750147687da4ef12343
Haochen Li <haochenli@appledeMBP-2.princeton.epam.com>
2017-10-10 14:34:07 +0800

    finish signup function

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,8 @@
 'use strict';
+let CryptoJS = require('crypto-js');
 let bodyParser = require('body-parser');
 const express = require('express');
-const url = 'mongodb://localhost:27017/JSA';// already defined;
+const url = 'mongodb://localhost:27017/epam';// already defined;
 const app = express();
 let mongodb = require('mongodb');
 let MongoClient = mongodb.MongoClient;
@@ -18,7 +19,6 @@ app.listen(3000, () => {
 });
 
 app.post('/api/signup', function(req, res) {
-  console.log(req.body);
   let sendContent = {
     'username': ' ',
     'email': '',
@@ -26,49 +26,16 @@ app.post('/api/signup', function(req, res) {
     'full name': '',
     'password': '',
   };
-  usernameCheck(req, res);
+  if (checkInfoValid(req, res, sendContent) === -1) {
+    console.log('check failed');
+    return;
+  }
 });
 
-function phonenumberCheck(req, res) {
-  if (req.body['phone number'] === '') {
-    let obj = {
-      'error': 'the phone number is void',
-    };
-    res.status(400).send(obj);
-    return -1;
-  }
-  if (! /^\d+$/.test(req.body['phone number'])) {
-    let obj = {
-      'error': 'the phone number is invalid',
-    };
-    res.status(400).send(obj);
-    return -1;
-  }
-  MongoClient.connect(url, function(err, db) {
-    if (err) {
-      let obj = {
-        'error': 'something went wrong',
-      };
-      res.status(500).send(obj);
-      db.close();
-      return -1;
-    }
-    db.collection('user').find({'username': req.body.username})
-      .toArray(function(err, items) {
-        if (items.length !== 0) {
-          let obj = {
-            'error': 'phone number conflicts!',
-          };
-          res.status(409).send(obj);
-          db.close();
-          return -1;
-        }
-        db.close();
-        fullnameCheck(req, res);
-      });
-  });
+function checkInfoValid(req, res, sendContent) {
+  return usernameCheck(req, res, sendContent);
 }
-function usernameCheck(req, res) {
+function usernameCheck(req, res, sendContent) {
   if (req.body.username.replace(' ', '') === '') {
     let obj = {
       'error': 'the username is void',
@@ -76,13 +43,13 @@ function usernameCheck(req, res) {
     res.status(400).send(obj);
     return -1;
   }
-  if (hasNumber(req.body.username)) {
-    let obj = {
-      'error': 'the username is invalid',
-    };
-    res.status(400).send(obj);
-    return -1;
-  }
+  // if (hasNumber(req.body.username)) {
+  //   let obj = {
+  //     'error': 'the username is invalid',
+  //   };
+  //   res.status(400).send(obj);
+  //   return -1;
+  // }
   MongoClient.connect(url, function(err, db) {
     if (err) {
       let obj = {
@@ -95,22 +62,20 @@ function usernameCheck(req, res) {
     }
     db.collection('user').find({'username': req.body.username})
       .toArray(function(err, items) {
-        console.log(items.length);
         if (items.length !== 0) {
           let obj = {
             'error': 'username conflicts!',
           };
-          console.log('~~~~~~~~~~~~~~~~~~~~~~~~~');
           res.status(409).send(obj);
           db.close();
           return -1;
         }
         db.close();
-        emailCheck(req, res);
+        emailCheck(req, res, sendContent);
       });
   });
 }
-function emailCheck(req, res) {
+function emailCheck(req, res, sendContent) {
   if (req.body.email.replace(' ', '') === '') {
     let obj = {
       'error': 'the email is void',
@@ -145,11 +110,50 @@ function emailCheck(req, res) {
           return -1;
         }
         db.close();
-        phonenumberCheck(req, res);
+        phonenumberCheck(req, res, sendContent);
       });
   });
 }
-function fullnameCheck(req, res) {
+function phonenumberCheck(req, res, sendContent) {
+  if (req.body['phone number'] === '') {
+    let obj = {
+      'error': 'the phone number is void',
+    };
+    res.status(400).send(obj);
+    return -1;
+  }
+  if (! /^\d+$/.test(req.body['phone number'])) {
+    let obj = {
+      'error': 'the phone number is invalid',
+    };
+    res.status(400).send(obj);
+    return -1;
+  }
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      let obj = {
+        'error': 'something went wrong',
+      };
+      res.status(500).send(obj);
+      db.close();
+      return -1;
+    }
+    db.collection('user').find({'username': req.body.username})
+      .toArray(function(err, items) {
+        if (items.length !== 0) {
+          let obj = {
+            'error': 'phone number conflicts!',
+          };
+          res.status(409).send(obj);
+          db.close();
+          return -1;
+        }
+        db.close();
+        fullnameCheck(req, res, sendContent);
+      });
+  });
+}
+function fullnameCheck(req, res, sendContent) {
   if (req.body['full name'].replace(' ', '') === '') {
     let obj = {
       'error': 'the full name is void',
@@ -164,12 +168,44 @@ function fullnameCheck(req, res) {
     res.status(400).send(obj);
     return -1;
   }
-  passwordCheck(req, res);
+  passwordCheck(req, res, sendContent);
 }
-function passwordCheck(req, res) {
+function passwordCheck(req, res, sendContent) {
+  if (req.body.password.length<=5) {
+    let obj = {
+      'error': 'the password is too short',
+    };
+    res.status(500).send(obj);
+    return -1;
+  }
   let encrypted = CryptoJS.AES.encrypt(req.body.password, 'Secret Passphrase');
-  let decrypted = CryptoJS.AES.decrypt(encrypted, 'Secret Passphrase');
-  console.log(decrypted.toString(CryptoJS.enc.Utf8));
+  sendContent.username = req.body.username;
+  sendContent.email = req.body.email;
+  sendContent['phone number'] = req.body['phone number'];
+  sendContent['full name'] = req.body['full name'];
+  sendContent.password = encrypted.toString();
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      let obj = {
+        'error': 'Something wrong!',
+      };
+      res.status(500).send(obj);
+      db.close();
+      return;
+    }
+    let objectId;
+    db.collection('user').insert(sendContent, function() {
+      objectId = sendContent._id;
+    });
+    res.set('location', '/api/signup'+objectId);
+    res.status(201).send();
+    console.log(sendContent);
+    db.close();
+  });
+  // let encrypted = CryptoJS.AES.
+  // encrypt(req.body.password, 'Secret Passphrase');
+  // let decrypted = CryptoJS.AES.decrypt(encrypted, 'Secret Passphrase');
+  // console.log(decrypted.toString(CryptoJS.enc.Utf8));
 }
 
 function validateEmail(email) {
