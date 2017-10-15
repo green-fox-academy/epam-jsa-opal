c9a0d6abc21ebe17083693699b8fd0da2cd8fffc
István Kárász <ikarasz@outlook.com>
2017-10-15 17:21:10 +0800

    Fixed eslint errors on config files | csslint added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+/* eslint no-magic-numbers: "off" */
+/* eslint max-len: "off" */
 module.exports = {
   'root': true,
   'env': {
@@ -11,7 +13,7 @@ module.exports = {
   'rules': {
     'arrow-body-style': ['error', 'as-needed'],
     'brace-style': ['error', '1tbs'],
-    'camelcase': ['error', { 'properties': 'always' }],
+    'camelcase': ['error', {'properties': 'always'}],
     'complexity': ['error', 5],
     'computed-property-spacing': ['error', 'never'],
     'func-style': ['error', 'declaration', {'allowArrowFunctions': true}],
@@ -20,7 +22,7 @@ module.exports = {
     'max-lines': ['error', 200],
     'max-nested-callbacks': ['error', 3],
     'max-params': ['error', 5],
-    'max-statements': ['error', 5, {'ignoreTopLevelFunctions': true }],
+    'max-statements': ['error', 5, {'ignoreTopLevelFunctions': true}],
     'no-else-return': 'error',
     'no-empty-function': 'error',
     'no-implicit-globals': 'error',
@@ -29,7 +31,7 @@ module.exports = {
     'no-loop-func': 'error',
     'no-magic-numbers': 'error',
     'no-multi-assign': 'error',
-    'no-multiple-empty-lines': ['error', {'max': 1, 'maxEOF': 1 }],
+    'no-multiple-empty-lines': ['error', {'max': 1, 'maxEOF': 1}],
     'no-new': 'error',
     'no-param-reassign': 'error',
     'no-return-assign': 'error',
@@ -39,11 +41,11 @@ module.exports = {
     'no-use-before-define': 'error',
     'padding-line-between-statements': [
       'error',
-       {blankLine: 'always', prev: ['const', 'let', 'var'], next: '*'},
-       {blankLine: 'any',    prev: ['const', 'let', 'var'], next: ['const', 'let', 'var']},
-       {blankLine: 'always', prev: ['directive'], next: ['*']},
-       {blankLine: 'always', prev: ['function'], next: ['function']},
-      ],
+      {blankLine: 'always', prev: ['const', 'let', 'var'], next: '*'},
+      {blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var']},
+      {blankLine: 'always', prev: ['directive'], next: ['*']},
+      {blankLine: 'always', prev: ['function'], next: ['function']},
+    ],
     'radix': 'error',
     'require-jsdoc': 'off',
     'semi-style': ['error', 'last'],
@@ -58,12 +60,12 @@ module.exports = {
     {
       'files': ['**/server/**/*.js'],
       'rules': {
-        'array-bracket-newline': ['error', { 'multiline': true }],
+        'array-bracket-newline': ['error', {'multiline': true}],
         'callback-return': 'error',
         'handle-callback-err': ['error', 'err'],
         'no-console': 'off',
-        'no-mixed-requires': ['error', { 'grouping': true }],
-        'object-curly-newline': ['error', { 'multiline': true}],
+        'no-mixed-requires': ['error', {'grouping': true}],
+        'object-curly-newline': ['error', {'multiline': true}],
         'strict': ['error', 'global'],
       },
     },
@@ -89,10 +91,10 @@ module.exports = {
         },
       },
       'rules': {
-        'array-bracket-newline': ['error', { 'multiline': true }],
+        'array-bracket-newline': ['error', {'multiline': true}],
         'jsx-quotes': ['error', 'prefer-double'],
         'no-alert': 'error',
-        'object-curly-newline': ['error', { 'multiline': true}],
+        'object-curly-newline': ['error', {'multiline': true}],
         'react/jsx-uses-vars': 'error',
         'react/jsx-uses-react': 'error',
       },
