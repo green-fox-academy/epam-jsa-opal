beb4b491f953458279c80a9c2964ecbcda7d9978
Haochen Li <haochenli@appledeMBP-2.princeton.epam.com>
2017-10-16 12:43:11 +0800

    confilct fixed

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,10 @@
 'use strict';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './components/header';
+import App from './App';
+import './index.scss';
 
 ReactDOM.render(<NavBar />, document.getElementById('root'));
 
