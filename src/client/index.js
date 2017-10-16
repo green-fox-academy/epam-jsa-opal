a9495090a218262fff30e99d7bec25105e2da935
Haochen Li <haochenli@appledeMBP-2.princeton.epam.com>
2017-10-16 13:42:14 +0800

    fixed issue

--- src/client/index.js
+++ src/client/index.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './components/header';
-import App from './App';
 import './index.scss';
 
 ReactDOM.render(<NavBar />, document.getElementById('root'));
