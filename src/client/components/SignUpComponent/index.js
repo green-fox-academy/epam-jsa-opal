72cec60e9865b8e78c566fa1c923823a48f59d26
zhengnan zhang <zhengnanzhang@yanans-MBP.princeton.epam.com>
2017-10-17 14:05:49 +0800

    not finish

--- src/client/components/SignUpComponent/index.js
+++ src/client/components/SignUpComponent/index.js
@@ -25,89 +25,134 @@ class SignUpComponent extends React.Component {
       'errorPassword': false,
     };
     ev.preventDefault();
-    let userName = ev.target.elements.namedItem('username').value;
-    let email = ev.target.elements.namedItem('email').value;
-    let phone = ev.target.elements.namedItem('phone').value;
-    let fullName = ev.target.elements.namedItem('fullname').value;
-    let password = ev.target.elements.namedItem('password').value;
     let obj = {
-      'username': userName,
-      'email': email,
-      'phone number': phone,
-      'full name': fullName,
-      'password': password,
+      'username': ev.target.elements.namedItem('username').value,
+      'email': ev.target.elements.namedItem('email').value,
+      'phone number': ev.target.elements.namedItem('phone').value,
+      'full name': ev.target.elements.namedItem('fullname').value,
+      'password': ev.target.elements.namedItem('password').value,
     };
     let jsonData = JSON.stringify(obj);
+
+    this.sendData(jsonData);
+  }
+
+  handleUserconflicterror(errorText) {
+    if (errorText === 'username conflicts!') {
+      this.setState({'errorUsername': true});
+    }
+    if (errorText === 'email conflicts!') {
+      this.setState({'errorEmail': true});
+    }
+    if (errorText === 'phone number conflicts!') {
+      this.setState({'errorPhone': true});
+    }
+  }
+
+  handleUsererror(errorText) {
+    if (errorText === 'the username is void') {
+      this.setState({'errorUsername': true});
+    }
+  }
+  handleEmailerror(errorText) {
+    if (errorText === 'the email is void') {
+      this.setState({'errorEmail': true});
+    }
+    if (errorText === 'the email format is invalid') {
+      this.setState({'errorEmail': true});
+    }
+  }
+  handlePhoneerror(errorText) {
+    if (errorText === 'the phone number is void') {
+      this.setState({'errorPhone': true});
+    }
+    if (errorText === 'the phone number is invalid') {
+      this.setState({'errorPhone': true});
+    }
+  }
+  handleFullnameerror(errorText) {
+    if (errorText === 'the full name is void') {
+      this.setState({'errorFullname': true});
+    }
+    if (errorText === 'the full name is invalid') {
+      this.setState({'errorFullname': true});
+    }
+  }
+  handlePassworderror(errorText) {
+    if (errorText === 'the password is void') {
+      this.setState({'errorPassword': true});
+    }
+    if (errorText === 'the password is too short') {
+      this.setState({'errorPassword': true});
+    }
+  }
+  judge409Error(xhr) {
+    if (xhr.status === 409) {
+      const errorObj = JSON.parse(xhr.responseText);
+      const errorText = errorObj.error;
+
+      this.handleUserconflicterror(errorObj.error);
+
+      this.setState({'error': errorText});
+    }
+  }
+  judge400Error(xhr) {
+    if (xhr.status === 400) {
+      const errorObj = JSON.parse(xhr.responseText);
+      const errorText = errorObj.error;
+
+      this.handleUsererror(errorText);
+      this.handleEmailerror(errorText);
+      this.handlePhoneerror(errorText);
+      this.handleFullnameerror(errorText);
+      this.handlePassworderror(errorText);
+      this.setState({'error': errorText});
+    }
+  }
+  judge500Error(xhr) {
+    if (xhr.status === 500) {
+      let errorText = JSON.parse(xhr.responseText).error;
+
+      this.setState({'error': errorText});
+    }
+  }
+
+  sendData(jsonData) {
     let xhr = new XMLHttpRequest();
+
     xhr.addEventListener('readystatechange', function() {
       if (xhr.readyState === XMLHttpRequest.DONE) {
         this.setState({'status': 'being'});
-        if (xhr.status === 400) {
-          let errorText = JSON.parse(xhr.responseText).error;
-          if (errorText === 'the username is void') {
-            this.setState({'errorUsername': true});
-          }
-          if (errorText === 'the email is void') {
-            this.setState({'errorEmail': true});
-          }
-          if (errorText === 'the email format is invalid') {
-            this.setState({'errorEmail': true});
-          }
-          if (errorText === 'the phone number is void') {
-            this.setState({'errorPhone': true});
-          }
-          if (errorText === 'the phone number is invalid') {
-            this.setState({'errorPhone': true});
-          }
-          if (errorText === 'the full name is void') {
-            this.setState({'errorFullname': true});
-          }
-          if (errorText === 'the full name is invalid') {
-            this.setState({'errorFullname': true});
-          }
-          if (errorText === 'the password is void') {
-            this.setState({'errorPassword': true});
-          }
-          if (errorText === 'the password is too short') {
-            this.setState({'errorPassword': true});
-          }
-          this.setState({'error': errorText});
-        }
-        if (xhr.status === 409) {
-          let errorText = JSON.parse(xhr.responseText).error;
-          if (errorText === 'username conflicts!') {
-            this.setState({'errorUsername': true});
-          }
-          if (errorText === 'email conflicts!') {
-            this.setState({'errorEmail': true});
-          }
-          if (errorText === 'phone number conflicts!') {
-            this.setState({'errorPhone': true});
-          }
-          this.setState({'error': errorText});
-        }
-        if (xhr.status === 500) {
-          let errorText = JSON.parse(xhr.responseText).error;
-          this.setState({'error': errorText});
-        }
+        this.judge409Error(xhr);
+        this.judge400Error(xhr);
+        this.judge500Error(xhr);
       }
     }.bind(this));
-    xhr.open('POST', '/api/signup');
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.setRequestHeader('Content-Type', 'application/json');
+    this.xhrSet(xhr);
     this.setState({'status': 'loading'});
     xhr.send(jsonData);
   }
 
+  xhrSet(xhr) {
+    xhr.open('POST', '/api/signup');
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+  }
+
   render() {
+    const {
+      status, error, errorUsername, errorEmail,
+      errorPhone, errorFullname, errorPassword,
+    } = this.state;
+
     return <SignUpForm
-      isLoading={this.state.status === 'loading'}
-      errorMessage={this.state.error}
-      errorUsername={this.state.errorUsername}
-      errorEmail={this.state.errorEmail}
-      errorPhone={this.state.errorPhone}
-      errorFullname={this.state.errorFullname}
-      errorPassword={this.state.errorPassword}
+      isLoading={status === 'loading'}
+      errorMessage={error}
+      errorUsername={errorUsername}
+      errorEmail={errorEmail}
+      errorPhone={errorPhone}
+      errorFullname={errorFullname}
+      errorPassword={errorPassword}
       onSubmit={this.submitHandler.bind(this)}
     />;
   }
