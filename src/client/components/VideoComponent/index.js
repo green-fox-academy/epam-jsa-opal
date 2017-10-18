f7b9f2ceb5d6577cf107c76d5b75be66192bf3d0
zhengnan zhang <zhengnanzhang@yanans-MBP.princeton.epam.com>
2017-10-18 15:40:12 +0800

    not finish

--- src/client/components/VideoComponent/index.js
+++ src/client/components/VideoComponent/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import VideoPlayer from '../VideoPlayerComponent';
-import Oval2 from './assets/Oval_2.png';
 
 class VideoComponent extends React.Component {
   render() {
     return (
-      <div className="VideoComponent">
+      <div className="video-component">
         <VideoPlayer />
-        <div className="VideoBelow">
-          <div className="VideoInfo">
-            <span>Midnight Starr - Slow Jam</span>
-            <span>4,250,633 views</span>
-          </div>
-          <div className="UserInfo">
-
-          </div>
-        </div>
       </div>
     );
   }
 }
+
 export default VideoComponent;
