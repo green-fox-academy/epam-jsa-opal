925cfc0b7e0b019d6a4b56b95479d580d90331d1
Haochen Li <haochenli@appledeMBP-2.princeton.epam.com>
2017-10-18 15:45:03 +0800

    navigation bar added

--- src/client/components/Home/index.js
+++ src/client/components/Home/index.js
@@ -3,14 +3,18 @@
 import React from 'react';
 import Header from '../header';
 import NavigationBar from '../NavigationBar';
+import subscription_1 from '../NavigationBar/assets/subscription_1.png';
+import subscription_2 from '../NavigationBar/assets/subscription_2.png';
+import subscription_3 from '../NavigationBar/assets/subscription_3.png';
+import subscription_4 from '../NavigationBar/assets/subscription_4.png';
 
 class Home extends React.Component {
   render() {
     return (
-      <main>
+      <div>
         <Header />
-        <NavigationBar link1="www.google.com"/>
-      </main>
+        <NavigationBar subscription_1={subscription_1} subscription_2={subscription_2} subscription_3={subscription_3} subscription_4={subscription_4}/>
+      </div>
     );
   }
 }
