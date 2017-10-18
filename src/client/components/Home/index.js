32338dc668d38d629eb03fb127c0c77a667dbdac
Haochen Li <haochenli@appledeMacBook-Pro-2.local>
2017-10-18 20:51:07 +0800

    issue fixed

--- src/client/components/Home/index.js
+++ src/client/components/Home/index.js
@@ -3,17 +3,13 @@
 import React from 'react';
 import Header from '../header';
 import NavigationBar from '../NavigationBar';
-import subscription_1 from '../NavigationBar/assets/subscription_1.png';
-import subscription_2 from '../NavigationBar/assets/subscription_2.png';
-import subscription_3 from '../NavigationBar/assets/subscription_3.png';
-import subscription_4 from '../NavigationBar/assets/subscription_4.png';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <NavigationBar subscription_1={subscription_1} subscription_2={subscription_2} subscription_3={subscription_3} subscription_4={subscription_4}/>
+        <NavigationBar />
       </div>
     );
   }
--- src/client/components/NavigationBar/index.js
+++ src/client/components/NavigationBar/index.js
@@ -3,29 +3,52 @@
 import React from 'react';
 import './index.scss';
 import {Link} from 'react-router-dom';
-import setting from './assets/setting.jpg'
+import setting from './assets/setting.jpg';
+import subscription_1 from '../NavigationBar/assets/subscription_1.png';
+import subscription_2 from '../NavigationBar/assets/subscription_2.png';
+import subscription_3 from '../NavigationBar/assets/subscription_3.png';
+import subscription_4 from '../NavigationBar/assets/subscription_4.png';
 
-export default (p) => (
-    <nav className = 'navigationBar'> 
-      <Link className='homeButton' to='/home'>home</Link>
-      <Link className='trendingButton' to='/trending'>Trending</Link>
-      <Link className='feedButton' to='/feed'>feed</Link>
-      <Link className='historyButton' to='/history'>History</Link>
-      <Link className='watchButton' to='/wahtchlater'>Watch later</Link>
-      <Link className='likedButton' to='/liked'>Liked</Link>
-      <div className = 'obstaclehorizantal'></div>
-      <span>subscription</span>
-      <Link className = 'subscription_1' to='/sub1'><img src={p.subscription_1}></img></Link>
-      <Link className = 'subscription_2' to='/sub1'><img src={p.subscription_2}></img></Link>
-      <Link className = 'subscription_3' to='/sub1'><img src={p.subscription_3}></img></Link>
-      <Link className = 'subscription_4' to='/sub1'><img src={p.subscription_4}></img></Link>
-      
-      <Link className='setting' to='/setting'>setting</Link>
-    </nav>
-
-
-);
-
-
-
-
+class NavigationBar extends React.Component {
+    render() {
+      let subscription = this.props.subscriptions.map(function(channel) {
+          return(
+            <Link className='subscription' key={channel.id} to={channel.url}>< img src={channel.id}></img></Link>
+          );
+      })
+      return (
+      <nav className = 'navigationBar'>
+        <Link className='homeButton' to='/home'>Home</Link>
+        <Link className='trendingButton' to='/trending'>Trending</Link>
+        <Link className='feedButton' to='/feed'>Feed</Link>
+        <Link className='historyButton' to='/history'>History</Link>
+        <Link className='watchButton' to='/wahtchlater'>Watch later</Link>
+        <Link className='likedButton' to='/liked'>Liked</Link>
+        <div className = 'obstaclehorizantal'></div>
+        <span>subscription</span>
+        {subscription}
+        <Link className='setting' to='/setting'>setting</Link>
+      </nav>
+      );
+    }
+};
+NavigationBar.defaultProps = {
+  test:1,
+    subscriptions: [{
+        avatar: '/sub1',
+        url: '/111',
+        id: subscription_1
+    },
+    {
+      avatar: '/sub1',
+      url: '/222',
+      id: subscription_2
+    },
+    {
+      avatar: '/sub1',
+      url: '/333',
+      id: subscription_3
+    }
+  ]
+};
+export default NavigationBar;
