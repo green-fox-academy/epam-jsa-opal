b79d04fc05a7f4d8fa22813f70c4baf014de4015
ZoeCN <290241128@qq.com>
2017-10-19 17:42:37 +0800

    delete test

--- src/client/App.js
+++ src/client/App.js
@@ -3,14 +3,13 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Signup from '../client/screen/SignUpScreen';
 import Login from '../client/components/Login';
 import Home from '../client/components/Home';
-import Comments from '../client/components/Comments';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <div>
-          <Route exact path="/" component={Comments} />
+          <Route exact path="/" component={Signup} />
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
           <Route path="/home" component={Home} />
