1ca49aae0e4b4ad108aca950c1fc9546c6e2d482
zhengnan zhang <zhengnanzhang@yanans-MBP.princeton.epam.com>
2017-10-19 18:37:12 +0800

    OPL-40

--- src/client/components/VideoComponent/index.js
+++ src/client/components/VideoComponent/index.js
@@ -1,45 +1,60 @@
 import React from 'react';
 import './index.scss';
 import userImg from './assets/Oval_2.png';
-import thumbUp from './assets/Fill 5.png';
 import share from './assets/share.png';
 import VideoPlayer from '../VideoPlayerComponent/index';
 
 class VideoComponent extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      'changeupColor': false,
+      'changedownColor': false,
+    };
   }
-
+  changeupColor() {
+    this.setState({'changeupColor': !this.state.changeupColor});
+  }
+  changedownColor() {
+    this.setState({'changedownColor': !this.state.changedownColor});
+  }
   render() {
     return (
       <div className="video-component">
         <VideoPlayer />
         <div className="video-below">
           <div className="video-info">
-            <span>Midnight Starr - Slow Jam</span>
-            <img className="share" src={share}></img>
-            <span>4,250,633 views</span>
+            <div className="video-name">
+              <span>Midnight Starr - Slow Jam</span>
+              <img className="share" src={share}></img>
+            </div>
+            <div>
+              <span className="view-num">4,250,633</span> <span>views</span>
+            </div>
           </div>
           <div className="user-info">
-            <img className="user-img" src={userImg}></img>
-            <div className="name-publishtime">
-              <span>Chill boy</span>
-              <span>Published on Jun 222,008</span>
-            </div>
-            <button className="subscribe">
+            <div className="userinfo-left">
+              <img className="user-img" src={userImg}></img>
+              <div className="name-publishtime">
+                <span className="user-name">Chill boy</span>
+                <span>Published on Jun 222,008</span>
+              </div>
+              <div className="subcribe-number">
+                <button className="subscribe">
               SUBSCRIBE
-            </button>
-            <span>3.8k</span>
+                </button>
+                <span className="subscribe-num">3.8k</span>
+              </div>
+            </div>
             <div className="thumb">
-              <button className="thumb-up">
-                <img className="thumbup-logo" src={thumbUp}></img>
-                like
-                <span className="like-num">5k+</span>
+              <button className="thumb-up" onClick={this.changeupColor.bind(this)}
+                className={this.state.changeupColor ? 'changeupcolor thumb-up' : 'thumb-up'}
+              >
+                like 5k+
               </button>
-              <button className="thumb-down">
-                <img className="thumbdown-logo" src={thumbUp}></img>
+              <button className="thumb-down" onClick={this.changedownColor.bind(this)}
+                className={this.state.changedownColor ? 'changedowncolor thumb-down' : 'thumb-down'}>
                 dislike
-                <span className="dislike-num"></span>
               </button>
             </div>
           </div>
