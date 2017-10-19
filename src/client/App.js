9684ae38e829390050f7f8a88d0e11830786070e
zhengnan zhang <zhengnanzhang@yanans-MBP.princeton.epam.com>
2017-10-19 10:52:54 +0800

    OPL-39

--- src/client/App.js
+++ src/client/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Signup from '../client/screen/SignUpScreen';
 import Login from '../client/components/Login';
-import Video from '../client/components/VideoPlayerComponent';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <div>
-          <Route exact path="/" component={Video} />
+          <Route exact path="/" component={Signup} />
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
         </div>
