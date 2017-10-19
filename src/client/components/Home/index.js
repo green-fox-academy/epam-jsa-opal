c1bffd56dd99d1827227f416c6a3babc064581b5
Haochen Li <haochenli@appledeMBP-2.princeton.epam.com>
2017-10-19 11:29:50 +0800

    homepage combine

--- src/client/components/Home/index.js
+++ src/client/components/Home/index.js
@@ -3,13 +3,16 @@
 import React from 'react';
 import Header from '../header';
 import NavigationBar from '../NavigationBar';
+import SuggestedVideos from '../SuggestedVideos';
+import './index.scss';
 
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <Header />
-        <NavigationBar />
+      <div className='homecontainer'>
+        <Header className='header'/>
+        <NavigationBar className='navigationBar'/>
+        <div className='suggestedVideos'> <SuggestedVideos /> </div>
       </div>
     );
   }
