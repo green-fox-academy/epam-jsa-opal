878b9598b699f9a650ed5303d61b7f1a3cdb0136
ZoeCN <290241128@qq.com>
2017-10-29 18:38:10 +0800

    front end

--- src/client/screen/ProfileScreen/index.js
+++ src/client/screen/ProfileScreen/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './index.scss';
-import VideoLists from '../../components/SuggestedVideosComponent/SuggestedVideosView/index';
+import Uploads from '../../components/SuggestedVideosComponent/SuggestedVideosView';
 
 class ProfileScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       'tab': 'profile',
-      'modify': 'false',
+      'modify': false,
       'profileInfos': {},
     };
+    this.defalutProfileInfos = {};
     this.fetchProfileInfos = this.fetchProfileInfos.bind(this);
     this.onChangeFullName = this.onChangeFullName.bind(this);
     this.onChangeAvatar = this.onChangeAvatar.bind(this);
@@ -17,7 +18,8 @@ class ProfileScreen extends React.Component {
   }
   componentDidMount() {
     this.fetchProfileInfos(this.props.location.search.split('=')[1], (profileInfos) => {
-      console.log('get infos');
+      this.defalutName = profileInfos.fullName;
+      this.defalutAvatar = profileInfos.avatar;
       this.setState({'profileInfos': profileInfos});
     });
   }
@@ -58,7 +60,7 @@ class ProfileScreen extends React.Component {
       'avatar': ev.target.elements.namedItem('avatar').value,
     };
 
-    this.setState({'modify': 'true'});
+    this.setState({'modify': true});
     fetch('/api/profile/' + ev.target.elements.namedItem('username').value, {
       method: 'put',
       headers: {
@@ -67,6 +69,7 @@ class ProfileScreen extends React.Component {
       },
       body: JSON.stringify(obj),
     }).then((response) => {
+      this.setState({'modify': false});
       statusCode = response.status;
       return response.json();
     }).then((result) => {
@@ -77,8 +80,14 @@ class ProfileScreen extends React.Component {
       }
     });
   }
+  onCancel() {
+    let defaultInfos = this.defalutProfileInfos;
+
+    defaultInfos.fullName = this.defalutName;
+    defaultInfos.avatar = this.defalutAvatar;
+    this.setState({profileInfos: defaultInfos});
+  }
   render() {
-    console.log('render infos');
     return (
       <div className="userInfos">
         <div className="profile-preview">
@@ -101,7 +110,8 @@ class ProfileScreen extends React.Component {
           <input type="phoneNumber" name="phoneNumber" value={this.state.profileInfos.phoneNumber} disabled={true}/>
           <input type="text" name="fullName" value={this.state.profileInfos.fullName} onChange={this.onChangeFullName} required/>
           <input type="avatar" name="avatar" value={this.state.profileInfos.avatar} onChange={this.onChangeAvatar} required/>
-          <button type="submit">Modify</button>
+          <button type="submit" disabled={this.state.modify}>Modify</button>
+          <button type="button" className="cancel" onClick={this.onCancel.bind(this)}>Cancel</button>
         </form>
         <div className={this.state.tab === 'subscription' ?
           'my-subscriptions show' :
@@ -119,17 +129,34 @@ class ProfileScreen extends React.Component {
           'my-videos show' :
           'my-videos'} >
           <h1>My Videos</h1>
-          <ul>
-            <li>video1</li>
-            <li>video2</li>
-            <li>video3</li>
-            <li>video4</li>
-            <li>video5</li>
-          </ul>
+          <Uploads videoInfos={this.props.uploads} suggestedVideosNum={30}/>
         </div>
       </div>
     );
   }
 }
+
+ProfileScreen.defaultProps = {
+  uploads: [
+    {
+      'videoId': '59f5564ef4ab731448b260d2',
+      'videoSrc': 'https://static.videezy.com/system/resources/previews/000/005/501/original/Earth_Sun_over_top.mp4',
+      'previewSrc': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf5H7b5S-9semZ9Y7tLhEXJmfaiAEyedxZNdKUBTHiKa1f_4figA',
+      'title': 'BB',
+      'videoTime': '5:00',
+      'author': 'bug2',
+      'viewNumber': 0,
+    },
+    {
+      'videoId': '59f32fc922c1552410b5104b',
+      'videoSrc': 'http://www.html5videoplayer.net/videos/toystory.mp4',
+      'previewSrc': 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2F736x%2F1f%2F33%2F37%2F1f3337d46984b00569be6d6310e05d4b--famous-movie-posters-movie-covers.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fallystardy%2Fmovie-posters%2F&docid=uKHwNW2Z9J_QkM&tbnid=87cGlPf9r8usFM%3A&vet=10ahUKEwjWyZfQ7ZDXAhUJULwKHTlBAYMQMwiqAigqMCo..i&w=288&h=433&bih=840&biw=1583&q=movie%20poster&ved=0ahUKEwjWyZfQ7ZDXAhUJULwKHTlBAYMQMwiqAigqMCo&iact=mrc&uact=8',
+      'title': 'SS',
+      'videoTime': '5:00',
+      'author': 'bug1',
+      'viewNumber': 0,
+    },
+  ],
+};
 
 export default ProfileScreen;
