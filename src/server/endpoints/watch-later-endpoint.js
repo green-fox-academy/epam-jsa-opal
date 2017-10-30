aaa2e619d892aa6812127dcdc3f6cea634c3e1f6
zhengnan zhang <zhengnanzhang@yanans-MBP.princeton.epam.com>
2017-10-30 13:50:59 +0800

    OPL-75

---
+++ src/server/endpoints/watch-later-endpoint.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const usersDb = require('../collections/users-db');
+const tokensDb = require('../collections/tokens-db');
+
+function updateErrorhandle(res, result) {
+  if (result === 'Upadate Failed') {
+    res.status(500).json({'error': 'Update Failed'});
+    return;
+  }
+  if (result === undefined) {
+    res.status(500).json({'error': 'Unable connect database'});
+    return;
+  }
+}
+
+// function sendBadRequest(res, message) {
+//   res.setHeader('content-type', 'application/json');
+//   res.status(400).send({'error': message});
+// }
+
+function watchLater(req, res) {
+  let token = req.get('Authorization');
+  let userId;
+
+  if (token === undefined) {
+    res.status(400).json({'error': 'unauthorization'});
+    return;
+  }
+  tokensDb.getToken(token, (tokenInfos) => {
+    userId = tokenInfos.userId;
+    let videoId = req.params.videoId;
+
+    console.log(videoId);
+    usersDb.findUserInfoById(userId, (userInfos) => {
+      if (userInfos.email === undefined) {
+        res.status(404).json({'error': 'not found'});
+        return;
+      }
+      console.log(userInfos);
+      userInfos.watchlater.push({'videoId': videoId});
+
+      usersDb.updateUserInfo(userInfos, userId, (result) => {
+        updateErrorhandle(res, result);
+        res.status(200).json({});
+      });
+    });
+  });
+}
+
+module.exports = {watchLater: watchLater};
+
