e0f07256763ea4f79aa15b3d06f3c0f0a4580c12
Haochen Li <haochenli@appledeMBP-2.princeton.epam.com>
2017-10-31 11:55:14 +0800

    js:lint run

--- src/client/components/VideoFullViewComponent/VideosFullView/index.js
+++ src/client/components/VideoFullViewComponent/VideosFullView/index.js
@@ -3,40 +3,40 @@ import VideoPreview from '../../VideoPreviewComponent';
 
 const VideosFullView = (props) => {
   let renderElement = [];
-  if(props.pagetype === 'trending') {
+
+  if (props.pagetype === 'trending') {
     let order = props.videoInfos;
-    order.sort((a,b)=>{
-      return b.viewNumber - a.viewNumber;
-    });
+
+    order.sort((a, b) => b.viewNumber - a.viewNumber);
     renderElement = order.map((value, index) => {
-    return (
-      <VideoPreview
-        videoInfo={value}
-        key={value.videoId} />
+      return (
+        <VideoPreview
+          videoInfo={value}
+          key={value.videoId} />
       );
     });
 
-  }else if(props.pagetype === 'liked') {
+  } else if (props.pagetype === 'liked') {
     renderElement = props.videoInfos.filter((videoInfo) => {
       return videoInfo.likeStatus.some((likeStatus) => {
         return props.userId === likeStatus.userId && likeStatus.liked === true;
-      })
+      });
     }).map((filteredVideoInfo) => {
       return <VideoPreview
         videoInfo={filteredVideoInfo}
         key={filteredVideoInfo.videoId} /> 
-    })
+    });
 
-  }else if(props.pagetype === 'feed') {
+  } else if (props.pagetype === 'feed') {
     renderElement = props.videoInfos.filter((videoInfo) => {
-      return (videoInfo.author === props.username)
+      return (videoInfo.author === props.username);
     }).map((value)=>{
       return  <VideoPreview
              videoInfo={value}
              key={value.videoId} />
-    })
+    });
 
-  }else if (props.pagetype === 'history') {
+  } else if (props.pagetype === 'history') {
     if(props.history !== undefined){
       renderElement = props.videoInfos.filter((videoInfo)=>{
         return props.history.some((singleHistory)=>{
@@ -54,7 +54,7 @@ const VideosFullView = (props) => {
       renderElement = props.videoInfos.filter((videoInfo)=>{
         return props.watchlater.some((watchElement)=>{
           return videoInfo.videoId === watchElement.videoId;
-        })
+        });
       }).map((value)=>{
         return <VideoPreview
         videoInfo={value}
